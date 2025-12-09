0d73053fe3b289526676b71187fd53199abea1eb
rufaelwondie12 <wondieastatkie@gmail.com>
2025-12-09 11:33:43 +0300

    Carousel effect

--- my-amazon-clone-app/src/App.jsx
+++ my-amazon-clone-app/src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import CarouselEffect from "./components/Carousel/CarouselEffect";
 import Header from "./components/Header/Header";
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
     <>
       <div>
         <Header />
+        <CarouselEffect />
       </div>
     </>
   );
