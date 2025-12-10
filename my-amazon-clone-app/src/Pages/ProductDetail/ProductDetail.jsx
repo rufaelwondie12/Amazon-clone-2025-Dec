c7d1bbb3903af54024a062b34b8925df6f03afb2
rufaelwondie12 <wondieastatkie@gmail.com>
2025-12-10 16:34:50 +0300

    Loading functionality integration

--- my-amazon-clone-app/src/Pages/ProductDetail/ProductDetail.jsx
+++ my-amazon-clone-app/src/Pages/ProductDetail/ProductDetail.jsx
@@ -5,26 +5,31 @@ import { useParams } from "react-router-dom";
 import axios from "axios";
 import { productUrl } from "../../Api/endPoints";
 import ProductCard from "../../components/Product/ProductCard";
+import Loader from "../../components/Loader/Loader";
 
 function ProductDetail() {
   const { productId } = useParams();
   // console.log(productId);
   const [product, setProduct] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
+    setIsLoading(true);
     axios
       .get(`${productUrl}/products/${productId}`)
       .then((res) => {
         setProduct(res.data);
+        setIsLoading(false);
       })
       .catch((err) => {
         console.log(err);
+        setIsLoading(false);
       });
   }, []);
 
   return (
     <LayOut>
-      <ProductCard product={product} />
+      {isLoading ? <Loader /> : <ProductCard product={product} />}
     </LayOut>
   );
 }
