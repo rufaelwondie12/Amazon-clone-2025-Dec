0dfc5387d391bf5506721fe9fdb297da2b0bedf3
rufaelwondie12 <wondieastatkie@gmail.com>
2025-12-11 16:57:45 +0300

    All cart page functionality

--- my-amazon-clone-app/src/components/Header/Header.jsx
+++ my-amazon-clone-app/src/components/Header/Header.jsx
@@ -9,6 +9,9 @@ import { DataContext } from "../DataProvider/DataProvider";
 
 const Header = () => {
   const [{ basket }, dispatch] = useContext(DataContext);
+  const totalItem = basket?.reduce((amount, item) => {
+    return item.amount + amount;
+  }, 0);
   return (
     <section className={classes.fixed}>
       <section className={classes.header_container}>
@@ -67,7 +70,7 @@ const Header = () => {
           {/* for cart */}
           <Link to="/cart" className={classes.cart}>
             <LuShoppingCart size={25} />
-            <span>{basket.length}</span>
+            <span>{totalItem}</span>
           </Link>
         </div>
       </section>
