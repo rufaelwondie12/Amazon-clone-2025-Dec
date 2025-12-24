5fc24cf27d197b8efcfd29df8238781a34850f50
rufaelwondie12 <wondieastatkie@gmail.com>
2025-12-24 17:09:35 +0300

    build

--- my-amazon-clone-app/src/Utility/firebase.js
+++ my-amazon-clone-app/src/Utility/firebase.js
@@ -5,13 +5,6 @@ import "firebase/compat/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  // apiKey: "AIzaSyCZuaWx6NiHIA4rpWQMyPgXl8qr3wqz268",
-  // authDomain: "for-presentation-ac7cd.firebaseapp.com",
-  // projectId: "for-presentation-ac7cd",
-  // storageBucket: "for-presentation-ac7cd.firebasestorage.app",
-  // messagingSenderId: "174684633828",
-  // appId: "1:174684633828:web:722ae5743691ea69a98af1",
-
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
